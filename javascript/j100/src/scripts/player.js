010d4bfa60bfc091df9e01050123c014d6428d60
MartinKilonzo <martinkilonzo2@gmail.com>
2017-01-18 15:43:11 -0500

    time j100 15 Added video player which plays a queue of videos.

--- javascript/j100/src/scripts/player.js
+++ javascript/j100/src/scripts/player.js
@@ -1,34 +1,33 @@
+/*global YT */
 const iframe = require('https://www.youtube.com/iframe_api');
 
-console.log('Player loaded!');
-
 function initialize() {
-  console.log('woo');
-    // // Update the controls on load
-    // updateTimerDisplay();
-    // updateProgressBar();
-    //
-    // // Clear any old interval.
-    // clearInterval(time_update_interval);
-    //
-    // // Start interval to update elapsed time display and
-    // // the elapsed part of the progress bar every second.
-    // time_update_interval = setInterval(function () {
-    //     updateTimerDisplay();
-    //     updateProgressBar();
-    // }, 1000)
+  // // Update the controls on load
+  // updateTimerDisplay();
+  // updateProgressBar();
+  //
+  // // Clear any old interval.
+  // clearInterval(time_update_interval);
+  //
+  // // Start interval to update elapsed time display and
+  // // the elapsed part of the progress bar every second.
+  // time_update_interval = setInterval(function () {
+  //     updateTimerDisplay();
+  //     updateProgressBar();
+  // }, 1000)
 }
 
-var onYouTubeIframeAPIReady = () => {
-  console.log('hello');
+var videoPlayer;
+
+function onYouTubeIframeAPIReady() {
   videoPlayer = new YT.Player('video-placeholder', {
     videoId: 'LaSRU5m_7og',
     playerVars: {
       color: 'white',
-      playlist: 'taJ60kskkns,FG0fTKAqZ5g',
+      playlist: 'taJ60kskkns,FG0fTKAqZ5g,OElxUkigPUE,hgx7L9Nyfjw',
     },
     events: {
-      onReady: initialize,
-    },
+      onReady: initialize
+    }
   });
-};
+}
