9b7246920bfa49f9879ae2281aabf1e752192fad
MartinKilonzo <martinkilonzo2@gmail.com>
2017-01-29 17:06:21 -0500

    time r001 5 Logs populated in chronological order; Stage 3 complete

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -4,12 +4,7 @@ File.open('gitlog.txt', 'w') do |fileStream|
 
   logLines = `git log`.split(/commit [\w\d]{40}\s/)
 
-  puts logLines.length
-
   logLines.delete_if { |log| log.include? 'Author: Bob Webber <webber@csd.uwo.ca>' }
 
-  puts logLines.length
-  puts logLines
-  # logLines.each {|log| puts log}
-  logLines.each {|log| fileStream.syswrite(log)}
+  logLines.reverse!.each {|log| fileStream.syswrite(log)}
 end
