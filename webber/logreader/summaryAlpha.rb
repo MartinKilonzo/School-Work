5a512f265295ad22c320470454322f5c542aa268
Bob Webber <webber@csd.uwo.ca>
2017-01-29 15:31:31 -0500

    updating practice framework and running summary.rb

--- webber/logreader/summaryAlpha.rb
+++ webber/logreader/summaryAlpha.rb
@@ -161,19 +161,29 @@ class TimeLogReport
   def initialize(time_log)
     @time_log = time_log
     @report = Hash.new
-    @total = 0
+    @total = 0.0
+    @javascript_total = 0.0
+    @ruby_total = 0.0
+    @haskell_total = 0.0
     # hash tables explained https://ruby-doc.org/core-2.2.0/Hash.html
   end
   def analyze()
     @time_log.each do | entry |
        task_name = entry.task_name
-       minutes = entry.minutes
+       # http://stackoverflow.com/questions/5502761/why-is-division-in-ruby-returning-an-integer-instead-of-decimal-value
+       minutes = entry.minutes.to_f
        if @report.has_key?(task_name) then
          @report[task_name] = @report[task_name] + minutes
        else
          @report[task_name] = minutes
        end
-       @total = @total + minutes
+       @total += minutes
+       # http://stackoverflow.com/questions/18463727/how-can-i-check-for-first-letters-in-string-in-ruby
+       # https://ruby-doc.org/core-2.2.0/String.html#method-i-start_with-3F
+       # http://stackoverflow.com/questions/4500504/does-it-matter-if-a-conditional-statement-comes-before-or-after-the-expression
+       @javascript_total += minutes if task_name.start_with?("j")
+       @ruby_total += minutes if task_name.start_with?("r")
+       @haskell_total += minutes if task_name.start_with?("h")
     end
     self
   end
@@ -187,13 +197,25 @@ class TimeLogReport
   def printMark(config)
     puts "On " + config.date + " marking for previous week"
     total = @total
+    javascript_total = @javascript_total
+    ruby_total = @ruby_total
+    haskell_total = @haskell_total
     if total > config.max_week_time then
+       javascript_total = javascript_total * config.max_week_time / total
+       ruby_total = ruby_total * config.max_week_time / total
+       haskell_total = haskell_total * config.max_week_time / total
        total = config.max_week_time
     end
-    puts "javascript practice time for previous week " + (total.to_s)
-    puts "ruby practice time for previous week " + "0"
-    puts "haskell practice time for previous week " + "0"
-    puts "mark for previous week " + (((10.0 * total)/config.max_week_time).to_s) + " (out of 10)"
+    # https://ruby-doc.org/core-2.4.0/Float.html#method-i-round
+    javascript_total = javascript_total.round(config.float_precision)
+    ruby_total = ruby_total.round(config.float_precision)
+    haskell_total = haskell_total.round(config.float_precision)
+    total = ((10.0 * total)/config.max_week_time).round(config.float_precision)
+    puts "javascript practice time for previous week " + (javascript_total.to_s)
+    puts "ruby practice time for previous week " + (ruby_total.to_s)
+    puts "haskell practice time for previous week " + (haskell_total.to_s)
+    OldMarks.new(config).report(javascript_total, ruby_total, haskell_total)
+    puts "mark for previous week " + (total.to_s) + " (out of 10)"
   end
   def printGeneral(config)
     config.putsq "----- Time Log Report"
@@ -202,16 +224,45 @@ class TimeLogReport
       puts "total time spent on task " + task + " was " + (minutes.to_s)
     end
     config.putsq "total time on all tasks according to this log was " + (@total.to_s)
-    if @total > config.max_week_time then
+    total = @total
+    if total > config.max_week_time then
        config.putsq "Note: time reported in excess of 180 minutes does not count toward practice"
-       @total = config.max_week_time
+       total = config.max_week_time
     end
-    puts "the percentage of weekly required practice time spent so far is " + (((100.0 * @total)/180.0).to_s) + "% (target is 100.0%)"
+    total_percent = ((100.0 * total)/180.0).round(config.float_precision)
+    puts "the percentage of weekly required practice time spent so far is " + (total_percent.to_s) + "% (target is 100.0%)"
   end
 end
 
+class OldMarks
+    # created to support a report of practice work on a per language basis 
+    # so far this semester, invoked:
+    #  OldMarks.new(config).report(javascript_total, ruby_total, haskell_total)
+    
+    def initialize(config)
+       @skip_report = ! (config.old_marks_file)
+       return if @skip_report
+       old_marks_file = config.old_marks_file
+       old_marks_file_string = IO.read(old_marks_file)
+       @old_marks_file_contents = Array.new
+       old_marks_file_string.each_line do | line |
+         @old_marks_file_contents.push(line.delete("\n"))
+       end
+    end
+    def report(current_javascript_total, current_ruby_total, current_haskell_total)
+       return if @skip_report
+       @old_marks_file_contents.each do | line |
+          puts "@@ " + line
+       end
+       puts "@@ current_javascript_total=" + (current_javascript_total.to_s)
+       puts "@@ current_ruby_total=" + (current_ruby_total.to_s)
+       puts "@@ current_haskell_total=" + (current_haskell_total.to_s)
+    end
+end
+
+
 class SummaryConfig
-  attr_accessor :date, :report_type, :quiet, :error_out, :max_entry_time, :max_week_time, :git_from_file
+  attr_accessor :date, :report_type, :quiet, :error_out, :max_entry_time, :max_week_time, :git_from_file, :float_precision, :old_marks_file
   def initialize(arg_list)
     if arg_list.include?("mark") then
       @report_type = :mark
@@ -226,6 +277,12 @@ class SummaryConfig
     else
       @date = "unknown"
     end
+    if arg_list.any? { |value| /^oldmarks=/ =~ value } then
+      oldmarks_arg = arg_list.find { |value| /^oldmarks=/ =~ value }
+      @old_marks_file = oldmarks_arg.sub("oldmarks=","")
+    else
+      @old_marks_file = false
+    end
     if arg_list.include?("quiet") then
       @quiet = true
     else
@@ -238,8 +295,9 @@ class SummaryConfig
       @git_from_file = false
     end
     @error_out = STDERR
-    @max_entry_time = 60
-    @max_week_time = 180
+    @max_entry_time = 60.0
+    @max_week_time = 180.0
+    @float_precision = 3
   end
   def putsq(message)
     puts message unless @quiet
@@ -251,8 +309,6 @@ end
 
 config = SummaryConfig.new(ARGV)
 # http://alvinalexander.com/blog/post/ruby/how-read-command-line-arguments-args-script-program
-
-
 
 config.putsq "----- Beginning to process log file"
 config.putsq "----- First read known task list"
