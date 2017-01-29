206b525e884c3982a985f47f342698e69a804e12
MartinKilonzo <martinkilonzo2@gmail.com>
2017-01-29 17:30:25 -0500

    time r001 20 log can now be filtered by prject (or any other string) via the command line; Completed statge 5

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -1,10 +1,18 @@
-puts 'hello'
-
 File.open('gitlog.txt', 'w') do |fileStream|
 
+  # Get the git log and split it by commit (removing the commit line identifier)
   logLines = `git log`.split(/commit [\w\d]{40}\s/)
 
+  # Remove all commits by Bob Webber
   logLines.delete_if { |log| log.include? 'Author: Bob Webber <webber@csd.uwo.ca>' }
 
+  # If an argument has been specified, use that as a project filter for the logs
+  if ARGV[0] != nil
+    logLines.delete_if { |log| !log.include? ARGV[0] }
+  end
+
+  # Output the modified log in chronological ascending order
   logLines.reverse!.each {|log| fileStream.syswrite(log)}
+  puts logLines
+
 end
