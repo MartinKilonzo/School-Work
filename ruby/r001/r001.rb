b2a6369ce2ea961c0daf044d820dabc6066b53a1
MartinKilonzo <martinkilonzo2@gmail.com>
2017-01-29 16:37:06 -0500

    time r001 10 Split log by commit

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -1,7 +1,11 @@
-puts 'hello';
+puts 'hello'
 
 File.open('gitlog.txt', 'w') do |fileStream|
   log = `git log`
   puts log.length
+
+  logLines = log.split(/commit (.){40}\n/)
+  puts logLines.length
+  puts logLines[2]
   fileStream.syswrite(log)
 end
