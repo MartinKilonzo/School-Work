ef6ab992af4a17c091e0efe0807a4cb11512dcac
MartinKilonzo <martinkilonzo2@gmail.com>
2017-01-29 23:34:46 -0500

    time j002 20 Added Incorrect tally and made the scoring system more robust

--- javascript/j002/src/j002.js
+++ javascript/j002/src/j002.js
@@ -3,6 +3,9 @@
 $(document).ready(() => {
   let questionIndex = -1;
   let correctQuestions = 0;
+  let incorrectQuestions = 0;
+
+  let correct = true;
   // Math.random.seed();
   const getRandomQuestion = () => {
     const min = 0;
@@ -12,7 +15,23 @@ $(document).ready(() => {
     $('#answer').text('');
   };
 
+  const addCorrectQuestion = () => {
+    console.log(correctQuestions)
+    if (!correct) {
+      correct = true;
+      correctQuestions += 1;
+      $('#correct').text(correctQuestions);
+    }
+  };
+
+  const addIncorrectQuestion = () => {
+    incorrectQuestions += 1;
+    $('#incorrect').text(incorrectQuestions);
+  };
+
   const showNextQuestion = () => {
+    if (!correct) addIncorrectQuestion();
+    correct = false;
     getRandomQuestion();
     const question = examdatabase.questions[questionIndex].question;
     $('#question').text(question);
@@ -23,15 +42,13 @@ $(document).ready(() => {
     $('#answer').text(answer);
   };
 
-  const addCorrentQuestion = () => {
-    correctQuestions += 1;
-    $('#correct').text(correctQuestions);
-  };
-
   showNextQuestion();
-  addCorrentQuestion();
+
+  $('#correct').text(correctQuestions);
+  $('#incorrect').text(incorrectQuestions);
+
 
   $('#question').siblings('button').on('click', event => showNextQuestion());
   $('#answer').siblings('button').on('click', event => showNextAnswer());
-  $('#correct').siblings('button').on('click', event => addCorrentQuestion());
+  $('#correctbtn').on('click', event => addCorrectQuestion());
 });
