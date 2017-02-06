f35e3dbcbf8111dac9cd7de68eac4dadaf0d88af
MartinKilonzo <martinkilonzo2@gmail.com>
2017-02-05 19:23:53 -0500

    time r001 73 Filtered out non-task logs from the list

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -1,18 +1,29 @@
+def isTask(log)
+  line = log.split("\n    ")[-1]
+  return !line.nil? ? (line[0..3].include? 'time') : false
+end
+
 File.open('gitlog.txt', 'w') do |fileStream|
 
   # Get the git log and split it by commit (removing the commit line identifier)
-  logLines = `git log`.split(/commit [\w\d]{40}\s/)
+  logList = `git log`.split(/commit [\w\d]{40}\s/)
 
   # Remove all commits by Bob Webber
-  logLines.delete_if { |log| log.include? 'Author: Bob Webber <webber@csd.uwo.ca>' }
+  logList.delete_if { |log| log.include? 'Author: Bob Webber <webber@csd.uwo.ca>' }
 
   # If an argument has been specified, use that as a project filter for the logs
   if ARGV[0] != nil
-    logLines.delete_if { |log| !log.include? ARGV[0] }
+    logList.delete_if { |log| !log.include? ARGV[0] }
   end
 
+  # Remove all non-tasks
+  logList.delete_if { |log| !isTask(log) }
+
+  #TODO: Sort the list by task
+
+
   # Output the modified log in chronological ascending order
-  logLines.reverse!.each {|log| fileStream.syswrite(log)}
-  puts logLines
+  logList.reverse!.each {|log| fileStream.syswrite(log)}
+  puts logList
 
 end
