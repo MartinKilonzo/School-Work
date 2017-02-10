36fe3f30c41caa9bc65de13fb92a4eb2f96120ca
MartinKilonzo <martinkilonzo2@gmail.com>
2017-02-09 22:02:58 -0500

    Finished 1 and 2+bonus

---
+++ p1.m
@@ -0,0 +1,9 @@
+function [X_out, Y_out] = p1(X, Y, label1, label2)
+% This function gets all X observations with Y matching label 1 or label 2
+% Then replaces the lessor Y with 1 and greater Y with 2
+
+iRow = find(Y==label1 | Y==label2)
+X_out = X(iRow, :)
+
+cond = Y(iRow) == min(label1, label2)
+Y_out = cond.*1 + ~cond.*2
---
+++ p2.m
@@ -0,0 +1,11 @@
+function [err, CONF] = p2(C, T)
+[m, i] = max(C(:))
+[n, i] = max(T(:))
+m = max(m, n)
+
+err = sum(C == T)/max(size(C, 1), size(T, 1))
+
+[a, b, c] = unique([C == T C T], 'rows', 'stable')
+
+CONF = zeros(m)
+CONF(sub2ind([m, m], a(:,3), a(:,2))) = transpose(histcounts(c))
