47426ca949ebbaf6cd6d537b16c4b94666adb141
MartinKilonzo <martinkilonzo2@gmail.com>
2017-02-10 19:10:39 -0500

    Completed 5

--- A1.m
+++ A1.m
@@ -14,7 +14,32 @@
 % 
 % p3(X, Y, X_new, 1)
 
-w = [-2; 1; -3]
-X = [-1 -2; -1 -4; 3 2]
+% w = [-2; 1; -3]
+% X = [-1 -2; -1 -4; 3 2]
+% 
+% % p4(w, X)
 
-p4(w, X)
+% X = [1 2; -1 4; 3 2; 1 7; 3 5; 4 6; 9 8]
+% Y = [1; 1; 2; 2; 2; 1; 1]
+
+% [w, e] = p5(X, Y, 100)
+
+% [X_digit, Y_digit] = p1(X_train_full, Y_train_full, 5, 10)
+% [w, e] = p5(X_digit, Y_digit, 10000)
+
+% Training Error
+% iter 100: err = 0.3102
+% iter 1000: err = 0.2812
+% iter 10000: err = 0.1968
+
+% [X_digit, Y_digit] = p1(X_test_full, Y_test_full, 5, 10)
+% [w, e] = p5(X_digit, Y_digit, 10000)
+
+% Test Error
+% iter 100: err = 0.3089
+% iter 1000: err = 0.2546
+% iter 10000: err = 0.2310
+
+
+
+
---
+++ p5.m
@@ -0,0 +1,15 @@
+function [w, minErr] = p5(X_train, Y_train, iterNum)
+minErr = inf
+preds = []
+for iter = 1:iterNum
+    iterWeights = randn(size(X_train, 2) + 1, 1)
+    prediction = p4(iterWeights, X_train)    
+    [err, confMat] = p2(prediction, Y_train)
+    
+    if err < minErr
+        w = iterWeights
+        minErr = err
+    end
+end
+
+    
