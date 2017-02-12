5f0958472e96d4a50dc96b17fac23e4e1c03415d
MartinKilonzo <martinkilonzo2@gmail.com>
2017-02-12 18:37:37 -0500

    time r001 65 Completed 6, 7

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -1,3 +1,58 @@
+HTML_BASE = '<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  </head>
+  <body>
+    <div id="app">
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </body>
+</html>'
+
+HTML_APP = "id=\"app\">\n"
+
+# TODO: Format HTML list into a table
+# TODO: repalce newline chars in logListTOHTML with <br>
+# TODO: Load HTML_BASE from file
+
+
+
+class String
+  def stringInject(baseString, iInsertion, insertionString)
+    stringStart = self[0..iInsertion]
+    stringEnd = self[iInsertion..-1]
+    return stringStart + insertionString + stringEnd
+  end
+end
+
+
+
+def logListToHTML(logList)
+  html = "<ul>\n"
+
+  logList.each do |log|
+    # Clean up each log HTML
+    listItem = "<li>\n" + "<p>\n" + log + "</p>\n" + "</li>\n"
+    listItem.gsub! "\n\n", "\n"
+    html += listItem
+  end
+
+  html += "</ul>\n"
+
+  return html
+end
+
+
+
 def isTask(log)
   line = log.split("\n    ")[-1]
   return !line.nil? ? (line[0..3].include? 'time') : false
@@ -21,9 +76,14 @@ File.open('gitlog.txt', 'w') do |fileStream|
 
   #TODO: Sort the list by task
 
+  File.open("index.html", 'w') do |htmlFileStream|
+    iInsertion = HTML_BASE.index(HTML_APP) + HTML_APP.length
+    htmlFileStream.write(HTML_BASE.stringInject(HTML_BASE, iInsertion, logListToHTML(logList)))
+  end
+
 
   # Output the modified log in chronological ascending order
   logList.reverse!.each {|log| fileStream.syswrite(log)}
-  puts logList
+  # puts logList
 
 end
