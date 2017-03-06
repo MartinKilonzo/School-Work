4e3442b4b4351c93ce6c24235344c084a16af7d3
MartinKilonzo <martinkilonzo2@gmail.com>
2017-03-05 23:31:00 -0500

    time j010 65 Added symbols and floats; completed stage 7

--- javascript/j010/j010.js
+++ javascript/j010/j010.js
@@ -12,9 +12,13 @@ fs.readFile('./nums.txt', 'utf8', (err, data) => {
   if (err) throw err;
 
   const numList = [];
-  data.split('\n').forEach((line) => {
-    line.split(/(\s)+/).forEach((token) => {
-      if (token && token.match(/[\d,;|()[\]{}]/)) numList.push(getType(token));
+  data.split(/[\n\r]+/).forEach((line) => {
+    line.split(/\b\s+\b(?!=)/).forEach((token) => { // Apparently javascript doesnt support negative lookbehinds (?<!=)
+      // if (token && token.match(/[\d,;|()[\]{}]/)) numList.push(getType(token));
+      if (token.match(/\.\d+/)) numList.push(`f ${token}`);
+      else if (token.match(/[(),;[]|{}]/)) numList.push(`s ${token}`);
+      else if (token.match(/=/)) numList.push(`v ${token}`);
+      else if (token.match(/\d+/)) numList.push(`i ${token}`);
     });
   });
 
