2e51c47b335f2f0cb74b31cb52197c741387e029
MartinKilonzo <martinkilonzo2@gmail.com>
2017-03-14 19:54:26 -0400

    Completed P1

--- Code/FunctionStubs/applyFilter.m
+++ Code/FunctionStubs/applyFilter.m
@@ -1,3 +1,17 @@
-
-
 function outIm = applyFilter(im,F)
+  % filter = flip(flip(F, 1), 2);
+  filter = F;
+  outIm = [];
+  k = floor(size(filter) / 2);
+  image = padarray(im, k, 0);
+  for iRow = 1 + k:size(image, 1) - k
+      row = [];
+    for iCol = 1 + k:size(image, 2) - k
+      grid = image(iRow - k:iRow + k, iCol - k:iCol + k);
+      pixel = sum(sum(grid .* filter));
+      row = [row pixel];
+    end
+    outIm = [outIm; row];
+  end
+  %outIm = transpose(reshape(outIm, size(im)));
+end
