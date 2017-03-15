79055dd44028ace842f0c60623d00f47662be340
MartinKilonzo <martinkilonzo2@gmail.com>
2017-03-15 16:48:23 -0400

    Completed 4 b)

--- Code/FunctionStubs/removeSeamV.m
+++ Code/FunctionStubs/removeSeamV.m
@@ -1,1 +1,16 @@
 function imOut = removeSeamV(im,seam)
+  imOut = [];
+  for iLayer=1:size(im, 3)
+    layer = [];
+    for iRow=1:size(im, 1)
+      row = [];
+      for iCol=1:size(im, 2)
+        if isempty(find(seam(iRow, :) == iCol))
+          row = [row im(iRow, iCol, iLayer)];
+        end
+      end
+      layer = [layer; row];
+    end
+    imOut = cat(3, imOut, layer);
+  end
+end
