c157bdaa6918179b68899dd24a54a3f6b0366100
MartinKilonzo <martinkilonzo2@gmail.com>
2017-03-14 20:41:11 -0400

    Removed unnecessary code

--- Code/FunctionStubs/applyFilter.m
+++ Code/FunctionStubs/applyFilter.m
@@ -1,8 +1,6 @@
 function outIm = applyFilter(im,F)
-  % filter = flip(flip(F, 1), 2);
-  filter = F
   outIm = [];
-  [u, v] = size(filter);
+  [u, v] = size(F);
   u = floor(u / 2);
   v = floor(v / 2);
   image = padarray(im, [u, v], 0);
@@ -10,10 +8,9 @@ function outIm = applyFilter(im,F)
       row = [];
     for iCol = 1 + v:size(image, 2) - v
       grid = image(iRow - u:iRow + u, iCol - v:iCol + v);
-      pixel = sum(sum(grid .* filter));
+      pixel = sum(sum(grid .* F));
       row = [row pixel];
     end
     outIm = [outIm; row];
   end
-  %outIm = transpose(reshape(outIm, size(im)));
 end
