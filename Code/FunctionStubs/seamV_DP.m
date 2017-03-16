a14a2e8818bccadff619837fe3f0920a2b92bb2d
MartinKilonzo <martinkilonzo2@gmail.com>
2017-03-15 23:35:09 -0400

    Completed 4 d)

--- Code/FunctionStubs/seamV_DP.m
+++ Code/FunctionStubs/seamV_DP.m
@@ -1,2 +1,18 @@
 
 function [M,P] = seamV_DP(E)
+  M = E(1, :);
+  P = ones(1, size(E, 2)) * -1;
+    for iRow=1:size(E, 1) - 1
+      row = M(iRow, :);
+      p = [];
+      for iCol=1:size(E, 2)
+        s = max(1, iCol - 1);
+        e = min(size(row, 2), iCol + 1);
+        r = row(s:e);
+        m = find(r==min(r)) + s - 1;
+        p = [p m];
+      end
+      P = [P; p];
+      M = [M; M(iRow, p) + E(iRow + 1, :)];
+  end
+end
