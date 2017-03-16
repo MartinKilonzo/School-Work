4f991c21767bc7db12ca3ee48919844e43efd68c
MartinKilonzo <martinkilonzo2@gmail.com>
2017-03-15 21:28:47 -0400

    Completed P4 c) and removed a loop in P3 b)

--- Code/FunctionStubs/addSeamV.m
+++ Code/FunctionStubs/addSeamV.m
@@ -1,2 +1,12 @@
 function imOut = addSeamV(im,seam)
-
+  imOut = [];
+  for iLayer=1:size(im, 3)
+    layer = [];
+    for iRow=1:size(im, 1)
+      row = im(iRow, :, iLayer);
+      row = [row(1:seam(iRow)) row(seam(iRow):end)];
+      layer = [layer; row];
+    end
+    imOut = cat(3, imOut, layer);
+  end
+end
--- Code/FunctionStubs/removeSeamV.m
+++ Code/FunctionStubs/removeSeamV.m
@@ -3,12 +3,8 @@ function imOut = removeSeamV(im,seam)
   for iLayer=1:size(im, 3)
     layer = [];
     for iRow=1:size(im, 1)
-      row = [];
-      for iCol=1:size(im, 2)
-        if isempty(find(seam(iRow, :) == iCol))
-          row = [row im(iRow, iCol, iLayer)];
-        end
-      end
+      row = im(iRow, :, iLayer);
+      row = [row(1:seam(iRow) - 1) row(seam(iRow) + 1:end)];
       layer = [layer; row];
     end
     imOut = cat(3, imOut, layer);
