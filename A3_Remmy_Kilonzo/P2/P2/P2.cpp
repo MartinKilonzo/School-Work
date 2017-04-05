047cbfda5e4979367d0eeabdfb82fce3e8340755
MartinKilonzo <martinkilonzo2@gmail.com>
2017-04-04 22:45:25 -0400

    Removed unnecessary files

--- A3_Remmy_Kilonzo/P2/P2/P2.cpp
+++ A3_Remmy_Kilonzo/P2/P2/P2.cpp
@@ -6,66 +6,83 @@
 #include <unordered_map>
 using namespace std;
 
-#include "../../NgramsToStudents/Ngrams/fileRead.h"
-#include "../../NgramsToStudents/Ngrams/VectorHash.h"
+#include "../../Ngrams/fileRead.h"
+#include "../../Ngrams/VectorHash.h"
 
 
 
 int main(int argc, char const *argv[]) {
-	// If there are not enough args, return -1
-	if (argc < 5)
-		return -1;
+  // If there are not enough args, return -1
+  // if (argc < 5)
+  //   return -1;
+	//
+  // // Otherwise, collect the function parameters
+  // string fileName1 = argv[1];
+  // string fileName2 = argv[2];
+  // int n = atoi(argv[3]);
+  // bool isCommonNGrams = (bool) atoi(argv[4]);
 
-	// Otherwise, collect the function parameters
-	string fileName1 = argv[1];
-	string fileName2 = argv[2];
-	int n = atoi(argv[3]);
-	bool isCommonNGrams = (bool) atoi(argv[4]);
+  string fileName1 = "../../../InputOutput/p2_a.txt";
+  string fileName2 = "../../../InputOutput/p2_b.txt";
+  unsigned int n = atoi("5");
+  bool isCommonNGrams = (bool) atoi("0");
 
-	// Capture all tokens
-	vector<string> tokens1;
-	vector<string> tokens2;
-	read_tokens(fileName1, tokens1, false);
-	read_tokens(fileName2, tokens2, false);
+  // Capture all tokens
+  vector<string> tokens1;
+  vector<string> tokens2;
+  read_tokens(fileName1, tokens1, false);
+  read_tokens(fileName2, tokens2, false);
 
-	// Do some N-Gram
+  if (tokens1.size() < n) {
+    cout << "\nInput file '" << fileName1 << "' is too small to create any nGrams of size " << n;
+    return -1;
+  } else if (tokens2.size() < n) {
+    cout << "\nInput file '" << fileName2 << "' is too small to create any nGrams of size " << n;
+    return -1;
+  } else {
 
-	// Count token frequency
-	unordered_map<string, int> dictionary;
-	for (auto &token : tokens) {
-		if (dictionary.count(token)) {
-			dictionary[token] = dictionary[token] + 1;
+    unordered_map <vector<string>, int> database1;
+    unordered_map <vector<string>, int> database2;
+
+
+    for (auto itr = tokens1.begin(); itr != tokens1.end() - n; itr++) {
+      vector<string> nGram(n);
+
+      for(auto jtr = itr; jtr != itr + n + 1; jtr++) {
+        nGram.push_back(*jtr);
+      }
+
+      if (database1.count(nGram) == 0 ) {
+        database1[nGram] = 1;
+      } else{
+        database1[nGram] = database1[nGram] + 1;
+      }
+    }
+
+    for (auto itr = tokens2.begin(); itr != tokens2.end() - n; itr++) {
+      vector<string> nGram(n);
+
+      for(auto jtr = itr; jtr != itr + n + 1; jtr++) {
+        nGram.push_back(*jtr);
+      }
+
+			// Record only the relevant nGrams
+      if ((database1.count(nGram) == 0) == isCommonNGrams) {
+        if (database2.count(nGram) == 0) {
+          database2[nGram] = 1;
+        } else {
+          database2[nGram] = database2[nGram] + 1;
+        }
+      }
+    }
+
+		for (auto &itdb : database2) {
+			for (auto &itng : itdb.first)
+				std::cout << itng;
+			std::cout << ", " << itdb.second <<  '\n';
 		}
-		else {
-			dictionary[token] = 1;
-		}
-	}
+		std::cout << database1.size() << " " << database2.size() << " "<< '\n';
+  }
 
-	// Pass token, frequency pairs into a vector for sorting
-	vector<pair<string, int>> wordCounts;
-	wordCounts.reserve(dictionary.size());
-	for (auto &word : dictionary) {
-		wordCounts.push_back(word);
-	}
-
-	// Sort the vector by frequency
-	std::sort(wordCounts.begin(), wordCounts.end(),
-		[](pair<string, int> const &t1, pair<string, int> const &t2) {
-		return t1.second > t2.second; // or use a custom compare function
-	}
-	);
-
-	// Print the k-most frequent words
-	int n = 0;
-	float sum = 0;
-	for (auto word = wordCounts.begin(); word != wordCounts.end() && n < k; word++, n++) {
-		std::cout << word->first << ", " << word->second << '\n';
-		sum += word->second;
-	}
-
-	// Print the k-most frequent words as a ratio of the total number of words
-	std::cout << sum / tokens.size() * 100 << " %" << '\n';
-	// std::cout << n << " words are necessary for understanding " << k * 100 << "% of the text, representing " << float(n) / dictionary.size() * 100 << "% of the language." << '\n';
-
-	return 0;
+  return 0;
 }
